09f0d49f35408dad96deb2194e949ae7fac0224d
Evans <711hikaru@gmail.com>
2019-12-16 14:43:30 +0800

    根組件

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,55 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <header>
+        <router-link to="/">KKBOX排行榜</router-link>
+    </header>
     <router-view/>
+    <YTplayer :YTid="youtubeID"></YTplayer>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+  import YTplayer from 'components/YTplayer';
+  import {playerListMixins} from 'util/mixins';
+  export default {
+    mixins: [playerListMixins],
+    components: {
+      YTplayer
     }
   }
-}
+</script>
+
+<style lang="scss">
+@import 'styles/grid';
+  * {
+    margin: 0;
+    padding: 0;
+    list-style: none;
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: "微軟正黑體";
+    background-color: #8EC5FC;
+    background-image: linear-gradient(62deg, #8EC5FC 0%, #E0C3FC 100%);
+    color: #fff;
+  }
+
+  header {
+    position: fixed;
+    top: 0;
+    z-index: 999;
+    padding: 0 30px;
+    line-height: 50px;
+
+    a {
+      text-decoration: none;
+      font-size: 20px;
+      color: #fff;
+      transition: color .3s linear;
+      &:hover {
+        color: #9d65b5;
+      }
+    }
+  }
 </style>
